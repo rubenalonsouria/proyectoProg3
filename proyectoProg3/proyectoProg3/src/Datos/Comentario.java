1ee16d1385534c66a6380dd0645a0dbc30fd5581
srhar <srhar@DESKTOP-A2L1USN>
2023-10-27 18:58:43 +0200

    atributos comentario

--- proyectoProg3/proyectoProg3/src/Datos/Comentario.java
+++ proyectoProg3/proyectoProg3/src/Datos/Comentario.java
@@ -1,6 +1,10 @@
 package Datos;
 
+import java.time.LocalDate;
+
 public class Comentario extends Pelicula{
 	//Los comentarios los puede eliminar el administrador si no son adecuados
-
+	protected Usuario usuario;
+	protected String texto;
+	protected LocalDate fecha;
 }
