975e0b3de3dea40483d35c5c0bf095fbd62a7f3a
srhar <srhar@DESKTOP-A2L1USN>
2023-10-27 18:29:07 +0200

    Cambio de herencia y atributos

--- proyectoProg3/proyectoProg3/src/Datos/Sesion.java
+++ proyectoProg3/proyectoProg3/src/Datos/Sesion.java
@@ -1,8 +1,13 @@
 package Datos;
 
-public class Sesion extends Cine{
+import java.time.LocalDate;
+
+public class Sesion extends Peliculas{
 	//La sesion es de pelicula, qeu seran las horas en las que estara disponible
 	//Pero no se si mejor que fuera hija de cine.
 	// Igual mejor de cine
 
+	protected int numSala;
+	protected LocalDate fecha;
+	protected boolean variable3D;
 }
