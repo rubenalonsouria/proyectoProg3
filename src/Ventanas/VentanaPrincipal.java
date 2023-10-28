5d93e617d6d68cd3f22a814c0f6e231b593e92c7
srhar <srhar@DESKTOP-A2L1USN>
2023-10-28 23:08:45 +0200

    añado el logo y ajustes varios

--- src/Ventanas/VentanaPrincipal.java
+++ src/Ventanas/VentanaPrincipal.java
@@ -1,13 +1,18 @@
 package Ventanas;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class VentanaPrincipal extends JFrame {
 	protected JFrame ventanaPrincipal;
 	protected JButton iniciarSesion, registrarse;
+	protected JLabel labelLogo;
 
 	public VentanaPrincipal() {
 		iniciarSesion = new JButton("Iniciar Sesion");
@@ -28,10 +33,14 @@ public class VentanaPrincipal extends JFrame {
 
 		});
 
+		labelLogo = new JLabel(new ImageIcon("deustocinelogo.png"));
+		
 		add(iniciarSesion);
 		add(registrarse);
 		setVisible(true);
 		setTitle("DeustoCine");
+		add(labelLogo, BorderLayout.SOUTH);
+		pack();
 		setBounds(100, 100, 1200, 800);
 		setLayout(new FlowLayout()); // En el futuro lo pondremos null y lo pondremos manualmente
 		setLocationRelativeTo(null);
