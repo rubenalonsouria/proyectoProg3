bb6d8345fe7124db596bf143586759ea7ef5656b
srhar <srhar@DESKTOP-A2L1USN>
2023-10-28 16:02:30 +0200

    Creacion atributo en clase sale

--- src/Cine/Sala.java
+++ src/Cine/Sala.java
@@ -3,5 +3,6 @@ package Cine;
 import java.util.ArrayList;
 
 public class Sala extends Cine{
-	 protected ArrayList<Integer> numerosButacas;
+	 protected ArrayList<Integer> numerosButacas; //Lista de todos los numeros de butacs que hay en esta sala
+	 protected int numSala; //Numero de la sala (identificativo)
 }
