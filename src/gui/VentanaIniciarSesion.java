35f6ca2074b850c3d9a93043d68bd8ae98b68c72
Haritz <haritz.carvalho@opendeusto.es>
2023-11-04 15:34:35 +0100

    añadido el checkbox recuerdame (sin funcionalidad)

--- src/gui/VentanaIniciarSesion.java
+++ src/gui/VentanaIniciarSesion.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.io.FileReader;
@@ -8,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -19,9 +21,11 @@ public class VentanaIniciarSesion extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JFrame ventanaPricipal;
 	private JTextField textField; // Poner el nobre de cada componente con el que es
+	private Dimension dimension;
 	private JLabel lblNewLabel, lblNewLabel_1;
 	private JPasswordField password;
 	private JButton btnNewButton, btnNewButton_1;
+	private JCheckBox recuerdameButton;
 	private Map<String, String> mapaUsuarios = new HashMap<String, String>();
 
 	public VentanaIniciarSesion(JFrame ventanaAnterior) {
@@ -29,10 +33,12 @@ public class VentanaIniciarSesion extends JFrame {
 		ventanaPricipal = ventanaAnterior;
 		lblNewLabel = new JLabel("Correo Electrónico:");
 		lblNewLabel_1 = new JLabel("Contraseña:");
-		textField = new JTextField("", 15);
-		password = new JPasswordField("", 15);
+		textField = new JTextField("");
+		password = new JPasswordField("");
+		dimension = new Dimension(25,15);
 		btnNewButton = new JButton("Iniciar");
 		btnNewButton_1 = new JButton("Volver");
+		recuerdameButton = new JCheckBox("Recuerdame", false);
 
 		// Para cancelar y volver a la ventana principal
 		btnNewButton_1.addActionListener((e) -> {
@@ -82,10 +88,14 @@ public class VentanaIniciarSesion extends JFrame {
 		
 		panelIniciarSesion.add(lblNewLabel);
 		panelIniciarSesion.add(textField);
+		textField.setPreferredSize(dimension);
 		panelIniciarSesion.add(lblNewLabel_1);
 		panelIniciarSesion.add(password);
+		panelAcciones.add(recuerdameButton);
 		panelAcciones.add(btnNewButton);
 		panelAcciones.add(btnNewButton_1);
+
+		
 
 		setVisible(true);
 		setTitle("Iniciar sesión");
