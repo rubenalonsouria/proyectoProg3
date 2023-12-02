bddaabc3b67480d0bc51f245e062d21cb7494d6a
Iker <iker.diez.perez@opendeusto.es>
2023-12-02 22:34:23 +0100

    Clase utilidades y intento de SGB

---
+++ src/main/CrearBaseDeDatosSQLite.java
@@ -0,0 +1,35 @@
+package main;
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+	import java.sql.Statement;
+
+	public class CrearBaseDeDatosSQLite {
+
+	    public static void main(String[] args) {
+	        // URL de conexión con la base de datos SQLite
+	        String url = "jdbc:sqlite:/sgb/mi_basededatos.db";
+
+	        // Intenta establecer la conexión con la base de datos
+	        try (Connection conn = DriverManager.getConnection(url)) {
+	            if (conn != null) {
+	                System.out.println("Conexión exitosa a la base de datos SQLite");
+	                
+	                // Sentencia para crear una tabla en la base de datos
+	                String sql = "CREATE TABLE IF NOT EXISTS Ejemplo (" +
+	                        "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+	                        "nombre TEXT NOT NULL," +
+	                        "edad INTEGER);";
+	                
+	                // Ejecuta la sentencia SQL
+	                Statement stmt = conn.createStatement();
+	                stmt.execute(sql);
+	                System.out.println("Tabla creada exitosamente");
+	            }
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        }
+	    }
+	}
+
