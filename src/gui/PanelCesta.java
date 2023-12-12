ac482c3706a5a473f9ac760b6c4acadec7c07bf3
Iker <iker.diez.perez@opendeusto.es>
2023-12-12 09:53:59 +0100

    panelCesta

--- src/gui/PanelCesta.java
+++ src/gui/PanelCesta.java
@@ -1,19 +1,57 @@
 package gui;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
 
 public class PanelCesta extends JPanel{
 
 	private static final long serialVersionUID = 1L;
+	private JList<String> jlist;
+	private DefaultListModel<String> modeloLista;
+	private JScrollPane scrollpanelList;
+	private JButton botonVolver, botonComprar;
 	
 public PanelCesta() {
+		
 		setLayout(new FlowLayout());
+		botonVolver = new JButton("Volver");
+		botonComprar = new JButton("Comprar");
 		
-		add(new JLabel("Aqui iria una tabla"));
+		modeloLista = new DefaultListModel<>();
+		jlist = new JList<>(modeloLista);
+		scrollpanelList = new JScrollPane(jlist);
+		jlist.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, Color.LIGHT_GRAY));
 		
 		
+		/*Lectura de la base de datos
+		 * diseñar la interfaz mejor:
+		 * hay una lista con peliculas agregadas al carrito  que contendran cada una un checkbox,
+		 * esas entradas son unicas ya que iran con el numero de asiento.
+		 * 
+		 * El checbox funcionara para seleccionar cuales quieres comprar y el boton compra te llevara a la pasarela de pago
+		 * */
+		
+//ActionListeners
+		botonVolver.addActionListener((e)-> {
+			
+		});
+		
+		botonComprar.addChangeListener((e)-> {
+			
+		});
+		
+		add(botonComprar);
+		add(botonVolver);
+		add(scrollpanelList);
 		setVisible(true);
 		setName("Cesta");
 	}
